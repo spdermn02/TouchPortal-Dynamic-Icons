59817a915dab693044af2e042f8e267edd73e9c7
spdermn02 <jameson.f.allen@gmail.com>
2022-01-09 12:39:22 -0600

    removed Webpack added Gauge Class (probably wrong) will need refactor fixing other minor things - spelling in entry.tp

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import TP from 'touchportal-api'
+import {Gauge} from './modules/interfaces'
+import { buildRoundGague } from './modules/gauge';
 import { pluginId } from './utils/consts'
-
 const TPClient = new TP.Client();
 
-TPClient.on("Action",(message:any,hold?:boolean) => {
+const dyanmicIconStates= { } as any;
+
+TPClient.on("Action",async (message:any,hold?:boolean) => {
     TPClient.logIt("INFO",`Action ${message.type} and hold is ${hold} to Touch Portal`)
+    if( message.actionId === 'generate_gauge_icon' ) {
+        const gauge = new Gauge();
+        gauge.value = message.data[7].value;
+        gauge.capStyle =  message.data[8].value;
+        gauge.indicatorColor = message.data[4].value;
+        gauge.highlightOn = message.data[5].value === "On" ? true : false;;
+        gauge.shadowColor = message.data[2].value === "On" ? message.data[3].value : '';
+        gauge.shadowOn = message.data[2].value === "On" ? true : false;
+        gauge.startingDegree = 0;
+        gauge.counterClockwise = false;
+        gauge.backgroundColor = message.data[9].value ;
+        const gaugeIcon = await buildRoundGague(256,256,gauge);
+
+        if( dyanmicIconStates[message.data[1].value] == undefined ) {
+            dyanmicIconStates[message.data[1].value] = 1;
+            TPClient.createState(message.data[1].value,message.data[1].value,'');
+        }
+        TPClient.stateUpdate(message.data[1].value,gaugeIcon);
+    }
 })
 
 TPClient.on("Info", (message?:any) => {
--- src/modules/interfaces.ts
+++ src/modules/interfaces.ts
@@ -14,4 +14,6 @@ export interface GradientGauge extends Gauge{
     gradientInfo: object
 }
 
+export class Gauge implements Gauge {
 
+}
