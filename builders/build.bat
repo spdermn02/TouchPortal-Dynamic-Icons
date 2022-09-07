5a23bcfd546d428f39ffbe969f7eb8f6a5c0fdff
Max Paperno <max@wdg.us>
2022-09-07 01:22:48 -0400

    [build] Update build scripts: * Include extra 'sharp' library dependencies with plugin distro (these cannot be build into the executable);     * Check comments in scripts regarding cross-platform builds; * Generate entry.tp as part of the build process (using new generator script from last commit); * Adds version number (when available) to the produced .tpp file names; * Refactor both build.* scripts to consolidate logic flow for both target platforms; * Move start.sh to 'base/TPDynamicIcons' folder to slightly simplify the build process. NOTE: build.sh not tested beyond basic syntax (on GNU bash w/out node/etc).

--- builders/build.bat
+++ builders/build.bat
@@ -1,48 +1,68 @@
 @echo off
 
-SET folder=TPDynamicIcons
-SET executable=touchportal-dynamic-icons
-SET target=%1
+:: Usage: build.bat <win|mac> [plugin.version.number]
+:: version number may also be in npm_package_version env. var
+:: set build_keep_bin env. var to "1" to keep the produced binary after archiving it
 
+SET target=%1
+SET version=%2
+
+SET base_dir=base
+SET plugin_name=TPDynamicIcons
+SET folder=%base_dir%\%plugin_name%
+SET executable=touchportal-dynamic-icons
+SET platform=MacOS
+SET npm_pkg_os=mac
+SET zip_args=
+SET version_sfx=
+
+if "%version%"=="" SET version=%npm_package_version%
+if not "%version%"=="" SET version_sfx=-v%version%
+
+if "%target%" == "win32" (
+    SET executable=%executable%.exe
+    SET platform=Windows
+    SET npm_pkg_os=win
+    SET zip_args=-xr!*.sh
+)
+
+SET tppfile=Installers\%plugin_name%-%platform%%version_sfx%.tpp
+
+echo === Generating entry.tp ===
+CMD /C node ./builders/gen_entry.js -v %version% -o %folder%
+
+echo;
+echo === Running TypeScript compilation ===
 CMD /C npm run tsc
 
-if %target% == win32 (
-    echo Building %executable%.exe for %target%
-    npm run pkg-win
+echo === Building %executable% for %target% ===
+CMD /C npm run pkg-%npm_pkg_os%
 
-    echo Moving %executable%.exe to base/%folder%
-    move %executable%.exe  base\%folder%
+echo;
+echo === Moving %executable% to %folder% ===
+move %executable% %folder%
 
-    echo Cleanup old %folder%-Windows.tpp files
-    del Installers\%folder%-Windows.tpp 
+:: Copy "sharp" library dependencies.
+:: Note that npm pkg warns of two node_modules/sharp folders to copy (build/Release and vendor/lib),
+::   but seems only one is required (the latter folder doesn't even exist).
+:: To build for Mac on Windows, must have it installed as additional platform:
+:: npm rebuild --platform=darwin --arch=x64 sharp
+echo;
+echo === Copying Sharp library files to %folder% ===
+xcopy /Y /I /R node_modules\sharp\build\Release %folder%\node_modules\sharp\build\Release
 
-    cd base 
+echo;
+echo === Cleanup old %tppfile% file ===
+del %tppfile%
 
-    echo Zipping up plugin
-    7z a -tzip ../Installers/%folder%-Windows.tpp %folder% 
+echo;
+echo === Zipping up plugin ===
+CMD /C "%PROGRAMFILES%\7-Zip\7z" a -tzip %tppfile% .\%base_dir%\* %zip_args%
 
-    echo Cleanup build process
-    erase /Q %folder%\%executable%.exe
-)
+echo;
+echo === Cleanup build process ===
+rd /S /Q %folder%\node_modules
+if not "%build_keep_bin%"=="1" del /Q %folder%\%executable%
 
-if %target% == mac (
-    echo Building %executable% for %target%
-    npm run pkg-mac
-
-    echo Moving %executable% to base/%folder%
-    move %executable%  base/%folder% 
-
-    echo Cleanup old %folder%-MacOs.tpp files
-    del Installers\%folder%-MacOs.tpp 
-
-    cd base 
-
-    echo Pulling in start.sh for plugin startup to work
-    copy start.sh %folder%
-
-    echo Zipping up plugin
-    7z a -tzip ../Installers/%folder%-MacOS.tpp %folder% 
-
-    echo Cleanup build process
-    erase /Q %folder%\*.sh %folder%\%executable% 
-)
+echo;
+echo === Build finished. ===
--- builders/build.sh
+++ builders/build.sh
@@ -1,50 +1,59 @@
 #!/bin/sh
 
-folder="TPDynamicIcons"
-executable="touchportal-dynamic-icons"
-target=$1
+# Usage: build.sh <win|mac> [plugin.version.number]
+# version number may also be in npm_package_version env. var
+# set build_keep_bin env. var to "1" to keep the produced binary after archiving it
 
+target=$1
+version=$2
+
+base_dir="base"
+plugin_name="TPDynamicIcons"
+folder="${base_dir}/${plugin_name}"
+executable="touchportal-dynamic-icons"
+platform="MacOS"
+npm_pkg_os="mac"
+zip_args=""
+version_sfx=""
+
+if [ ! "$version" -a -n "$npm_package_version" ]; then version=${npm_package_version}; fi
+if [ "$version" ]; then version_sfx="-v${version}"; fi
+
+if [ target = "win32" ]; then
+    executable="${executable}.exe"
+    platform="Windows"
+    npm_pkg_os="win"
+    zip_args="-x *.sh"
+fi
+
+tppfile="Installers/${plugin_name}-${platform}${version_sfx}.tpp"
+
+echo "=== Generating entry.tp ==="
+node ./builders/gen_entry.js -v ${version} -o ${folder}
+
+echo "=== Running TypeScript compilation ==="
 npm run tsc
 
-#windows build
-if target=="win32" then
-    echo "Building ${executable}.exe for $target"
-    npm run pkg-win
+echo "=== Building $executable for $target ==="
+npm run pkg-${npm_pkg_os}
 
-    echo "Moving ${executable}.exe to base/${folder}"
-    mv ${executable}.exe base/${folder}
+echo "=== Moving %executable% to ${folder} ==="
+mv ${executable} ${folder}
 
-    echo "echo Cleanup old ${folder}-Windows.tpp files"
-    rm Installers/${folder}-Windows.tpp 
+# Copy "sharp" library dependencies.
+# Note that npm pkg warns of two node_modules/sharp folders to copy (build/Release and vendor/lib),
+#   but seems only one is required (the latter folder doesn't even exist).
+# To build for Windows on Mac, must have it installed as additional platform:
+# npm rebuild --platform=windows --arch=x64 sharp
+echo "=== Copying Sharp library files to ${folder} ==="
+cp -r node_modules/sharp/build/Release ${folder}/node_modules/sharp/build/Release
 
-    cd base 
+echo "=== Cleanup old $tppfile file"
+rm -rf  ${tppfile}
 
-    echo "Zipping up plugin"
-    zip -r ../Installers/${folder}-Windows.tpp ${folder}
+echo "=== Zipping up plugin ==="
+zip -r ${zip_args} ${tppfile} ./${base_dir}/*
 
-    echo "Cleanup build process"
-    rm -rf ${folder}/*.sh ${folder}/${executable}
-fi
-
-#mac build
-if target=="mac" then
-    echo "Building ${executable} for $target"
-    npm run pkg-mac
-
-    echo "Moving ${executable} to base/${folder}"
-    mv ${executable} base/${folder}
-
-    echo "echo Cleanup old ${folder}-MacOs.tpp files"
-    rm -rf Installers/${folder}-MacOs.tpp
-
-    cd base 
-
-    echo "Pulling in start.sh for plugin startup to work"
-    cp start.sh ${folder}
-
-    echo "Zipping up plugin"
-    zip -r ../Installers/${folder}-MacOs.tpp ${folder}
-
-    echo "Cleanup build process"
-    rm -rf ${folder}/*.sh ${folder}/${executable}
-fi
+echo "=== Cleanup build process ==="
+rm -rf ${folder}/node_modules
+if [ "$build_keep_bin" != "1" ]; then rm -f ${folder}/${executable}; fi
