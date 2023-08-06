365886bc1b83ba60e3d0bd7640127effe91269cf
Max Paperno <max@wdg.us>
2023-08-06 15:15:29 -0400

    [elements][ShadowStyle] Add method to save and restore the "shadow context" (properties) of a canvas vs. just resetting them; Add clear() method.

--- src/modules/elements/ShadowStyle.ts
+++ src/modules/elements/ShadowStyle.ts
@@ -1,16 +1,27 @@
 import { IRenderable, RenderContext2D } from '../interfaces';
 import { Vect2d } from '../geometry';
-import { BrushStyle } from './';
 
 export default class ShadowStyle implements IRenderable {
     blur: number = 0;
     offset: Vect2d = new Vect2d();
-    color: BrushStyle = new BrushStyle("#0000");
+    color: string = "#0000";
+
+    savedContext:any = {
+        blur: 0,
+        offsetX: 0,
+        offsetY: 0
+    }
 
     // IRenderable
     get type(): string { return "ShadowStyle"; }
     // returns true if blur and offset are are <= 0 or if color is transparent/invalid.
-    get isEmpty(): boolean { return (this.blur <= 0 && this.offset.isEmpty); }
+    get isEmpty(): boolean { return (this.blur <= 0 && this.offset.isNull); }
+
+    /** Resets shadow coordinates to zero. Does not affect color. */
+    clear() {
+        this.blur = 0;
+        this.offset.set(0, 0);
+    }
 
     render(ctx: RenderContext2D): void {
         if (this.isEmpty)
@@ -21,10 +32,21 @@ export default class ShadowStyle implements IRenderable {
         ctx.shadowOffsetY = this.offset.y;
     }
 
-		/** Resets all shadow attributes on given context.  */
-		resetContext(ctx: RenderContext2D): void {
-        ctx.shadowBlur = 0;
-        ctx.shadowOffsetX = 0;
-        ctx.shadowOffsetY = 0;
-		}
+    /** Saves the given context's shadow coordinate properties to `ShadowStyle.savedContext` member.
+     * See also `restoreContext()`;
+     */
+    saveContext(ctx: RenderContext2D): void {
+        this.savedContext.blur = ctx.shadowBlur;
+        this.savedContext.offsetX = ctx.shadowOffsetX;
+        this.savedContext.offsetY = ctx.shadowOffsetY;
+    }
+
+    /** Resets all shadow attributes on given context to the values saved in `savedContext`.
+     * See also `saveContext()`.
+      */
+    restoreContext(ctx: RenderContext2D): void {
+        ctx.shadowBlur = this.savedContext.blur;
+        ctx.shadowOffsetX = this.savedContext.offsetX;
+        ctx.shadowOffsetY = this.savedContext.offsetY;
+    }
 }
