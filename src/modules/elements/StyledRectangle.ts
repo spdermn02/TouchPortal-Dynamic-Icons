72c5b58d90e0ab251ffb22e644c3ff08a9ca8dc2
Max Paperno <max@wdg.us>
2023-08-06 16:04:05 -0400

    [elements][StyledRectangle] Refactor and add new features: * A rectangle can now have a size (w and h) specified, in either relative (%) or absolute (px) units (size values from action data are dynamically evaluated); * Radius values can now be absolute as well as relative and are now dynamically evaluated; * Added an alignment property which is used when rectangle size doesn't fill the drawing area; * New behavior: Added option to adjust overall size to account for shadow dimensions so they don't overflow the drawing area (default is to adjust); * Uses new "smart shadow" renderPath() drawing method from DrawingStyle for cleaner shadow effect; * Refactor to make it more "subclass friendly" (used by upcoming new LinearProgressBar element); * Refactor action data parsing loop.

--- src/modules/elements/StyledRectangle.ts
+++ src/modules/elements/StyledRectangle.ts
@@ -1,49 +1,89 @@
 
 import { ILayerElement, RenderContext2D } from '../interfaces';
+import { Alignment } from '../enums';
 import { ParseState } from '../types';
-import { Rectangle, SizeType, Vect2d } from '../geometry';
+import { Rectangle, UnitValue } from '../geometry';
 import DrawingStyle from './DrawingStyle';
+import { evaluateValue, parseNumericArrayString } from '../../utils/helpers';
 import { Path2D } from 'skia-canvas'
 
 // Draws a rectangle shape on a canvas context with optional radii applied to any/all of the 4 corners (like CSS). The shape can be fully styled with the embedded DrawingStyle property.
 export default class StyledRectangle implements ILayerElement
 {
-    size: SizeType = { width: 0, height: 0 };
+    /** A zero width/height (default) indicates to draw into the full available image area (eg. passed to `render()` in `rect` argument). */
+    width: UnitValue = new UnitValue(0, "%");
+    height: UnitValue = new UnitValue(0, "%");
     style: DrawingStyle = new DrawingStyle();
-     // `radii` value is like css border-radius, 1-4 numbers starting at top left corner, etc; empty array for no radius;
-    // individual values are in percent of overall size, like the CSS <percentage> style border-radius value;
-    // see `radii` param at https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/roundRect#syntax
+    /** `radii` value is like css border-radius, 1-4 numbers starting at top left corner, etc; empty array for no radius;
+        individual values can be in either percent of overall size (like CSS border-radius %) or in absolute pixels.
+        See `radii` param at https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/roundRect#syntax  */
     radii: number[] = [];
+    radiusIsRelative: boolean = true;
+    /** How to align within drawing area if/when `width`/`height` doesn't fill it completely */
+    alignment: Alignment = Alignment.CENTER;
+    /** Whether to adjust (reduce) the overall drawing area size to account for shadow offset/blur. */
+    adjustSizeForShadow: boolean = true;
+
+    protected haveRadius: boolean = false;
 
     constructor(init?: Partial<StyledRectangle>) { Object.assign(this, init); }
     // ILayerElement
     get type() { return "StyledRectangle"; }
 
-    /** Returns true if there is nothing to draw: size is empty, colors are blank or transparent, or there is no fill and stroke width is zero */
+    /** Returns true if there is nothing to draw: there is no fill and stroke width is zero */
     get isEmpty(): boolean {
         return this.style.fill.isEmpty && this.style.line.isEmpty;
     }
 
+    protected parseRadius(value: string) {
+        this.haveRadius = parseNumericArrayString(value, this.radii = [], 4, 0);
+    }
+
     loadFromActionData(state: ParseState): StyledRectangle {
-        let styleParsed = false;
-        for (let i = state.pos, e = state.data.length; i < e; ++i) {
-            const dataType = state.data[i].id.split('rect_').at(-1);  // last part of the data ID determines its meaning
+        let styleParsed = false,
+            atEnd = false;
+        for (const e = state.data.length; state.pos < e && !atEnd; ) {
+            const data = state.data[state.pos];
+            const dataType = data.id.split('rect_').at(-1);  // last part of the data ID determines its meaning
+            if (!dataType)
+                break;
             switch (dataType) {
-                case 'radius': {
-                    const value = state.data[i].value.trim();
-                    if (value)
-                        this.radii = value.split(/\s*(?:,|\s)\s*/).map((r:string) => parseFloat(r.trim()) || 0);
+                case 'size_w': {
+                    const sz = evaluateValue(data.value);
+                    if (sz > 0)
+                        this.width.value = sz;
                     break;
                 }
+                case 'size_w_unit':
+                    this.width.setUnit(data.value);
+                    break;
+
+                case 'size_h': {
+                    const sz = evaluateValue(data.value);
+                    if (sz > 0)
+                        this.height.value = sz;
+                    break;
+                }
+                case 'size_h_unit':
+                    this.height.setUnit(data.value);
+                    break;
+
+                case 'radius':
+                    this.parseRadius(data.value);
+                    break;
+                case 'radius_unit':
+                    this.radiusIsRelative = UnitValue.isRelativeUnit(data.value);
+                    break;
+
                 default: {
-                    if (styleParsed || !dataType || !dataType.startsWith('style_')) {
-                        i = e;  // end loop
-                        continue;
-                    }
                     // any other fields should be styling data
-                    this.style.loadFromActionData(state);
-                    styleParsed = true;
-                    i = state.pos;
+                    if (!styleParsed && dataType.startsWith('style_')) {
+                        this.style.loadFromActionData(state);
+                        styleParsed = true;
+                    }
+                    else {
+                        atEnd = true;
+                    }
                     continue;
                 }
             }
@@ -54,46 +94,92 @@ export default class StyledRectangle implements ILayerElement
     }
 
     // ILayerElement
-    render(ctx: RenderContext2D, rect: Rectangle): void {
-        // console.dir(this);
-        if (!ctx || this.isEmpty)
-            return;
-        let size = this.size.width ? this.size : rect.size;
-        let offset = new Vect2d(rect.x, rect.y);  // drawing position, may need to be adjusted to allow for stroke width
-        ctx.save();
+    render(ctx: RenderContext2D, rect: Rectangle): void { this.renderImpl(ctx, rect); }
 
-        // set stroke scaling if needed
-        const havePen = !this.style.line.isEmpty;
-        if (havePen) {
-            // stroke line width is percentage of half the overall size where 100% would be half the smaller of width/height (and strokes would overlay the whole shape)
-            this.style.line.widthScale = Math.min(size.width, size.height) * .005;
+    /** The actual drawing implementation. May be used by subclasses.
+        Returns the area left over "inside" the rectangle after adjusting for stroke and/or shadow. */
+    protected renderImpl(ctx: RenderContext2D, rect: Rectangle): Rectangle
+    {
+        let window: Rectangle = rect.clone();  // the area to draw into, may need to be adjusted
+        if (this.isEmpty)
+            return window;
+
+        // If this instance specifies a size in either dimension, these override the drawing area size.
+        // We can also adjust the alignment within the drawing area if one of the final dimensions is smaller.
+        if (this.width.value && !(this.width.isRelative && this.width.value == 100)) {
+            window.width = this.width.isRelative ? this.width.value * .01 * rect.size.width : this.width.value;
+            if (window.width < rect.width) {
+                switch (this.alignment & Alignment.H_MASK) {
+                    case Alignment.HCENTER:
+                        window.x += (rect.width - window.width) * .5;
+                        break;
+                    case Alignment.RIGHT:
+                        window.x += rect.width - window.width;
+                        break;
+                }
+            }
+        }
+        if (this.height.value && !(this.height.isRelative && this.height.value == 100)) {
+            window.height = this.height.isRelative ? this.height.value * .01 * rect.size.height : this.height.value;
+            if (window.height < rect.height) {
+                switch (this.alignment & Alignment.V_MASK) {
+                    case Alignment.VCENTER:
+                        window.y += (rect.height - window.height) * .5;
+                        break;
+                    case Alignment.BOTTOM:
+                        window.y += rect.height - window.height;
+                        break;
+                }
+            }
+        }
+
+        // set stroke scaling and adjust drawing size if needed
+        let penW = 0;
+        if (!this.style.line.isEmpty) {
+            // relative stroke width is percentage of half the overall size where 100% would be half the smaller of width/height (and strokes would overlay the whole shape)
+            if (this.style.line.width.isRelative)
+                this.style.line.widthScale = Math.min(window.width, window.height) * .005;
             // adjust size to prevent clipping of stroke which is drawn middle-aligned on the shape border
-            const penW = this.style.line.scaledWidth;
-            size = {width: size.width - penW, height: size.height - penW};
-            // also need to offset the draw origin by half of the size delta
-            offset.add(penW * .5);
+            penW = this.style.line.scaledWidth;
+            window.adjust(penW * .5, -penW);
+        }
+        //console.debug("Size", this.size.toString(), "Rect", rect.toString(), "Window", window.toString(), "Pen", penW);
+
+        if (this.adjustSizeForShadow && !this.style.shadow.isEmpty) {
+            // adjust for shadow
+            const s = this.style.shadow,
+                penSz = this.style.strokeOver && !this.style.fill.isEmpty ? penW * .5: 0,
+                xOffs = Math.max(s.blur - s.offset.x - penSz, 0),
+                yOffs = Math.max(s.blur - s.offset.y - penSz, 0);
+                //adj = new Rectangle(xOffs, yOffs, -Math.max(xOffs + s.blur + s.offset.x - penSz, xOffs), -Math.max(yOffs + s.blur + s.offset.y - penSz, yOffs));
+            //console.debug("Shadow adjust", adj.toString(), "Window", window.toString());
+            //window.adjust(adj.origin, adj.size);
+            window.adjust(xOffs, yOffs, -Math.max(xOffs + s.blur + s.offset.x - penSz, xOffs), -Math.max(yOffs + s.blur + s.offset.y - penSz, yOffs));
         }
 
         // Note that for some reasons transforms only work when using a Path2D object, vs. adding rect/roundRect shapes to the context directly and using fill()/stroke().
         // And, specifically need the skia-canvas version since the built-in one doesn't have rect()/roundRect() (at least in the current ES version as of this writing).
         const path = new Path2D();
-        if (this.radii.length) {
-            const ratio = (size.width + size.height) * .5;  // this is a bit simplistic really
-            const radii = this.radii.map(r => r * .01 * ratio);
-            path.roundRect(offset.x, offset.y, size.width, size.height, radii);
+        if (this.haveRadius) {
+            if (this.radiusIsRelative) {
+                const ratio = Math.max((window.width + window.height) * .005, 0);  // this is a bit simplistic really
+                const radii = this.radii.map(r => r * ratio);
+                path.roundRect(window.x, window.y, window.width, window.height, radii);
+            }
+            else {
+                path.roundRect(window.x, window.y, window.width, window.height, this.radii);
+            }
         }
         else {
-            path.rect(offset.x, offset.y, size.width, size.height);
+            path.rect(window.x, window.y, window.width, window.height);
         }
 
-        // set canvas drawing style properties
-        this.style.render(ctx);
+        // draw the rectangle using our style
+        this.style.renderPath(ctx, path);
 
-        if (!this.style.fill.isEmpty)
-            ctx.fill(path);
-        if (havePen)
-            ctx.stroke(path);
+        if (penW)
+            window.adjust(penW * .5, -penW);
 
-        ctx.restore();
+        return window;
     }
 }
---
+++ src/modules/enums.ts
@@ -0,0 +1,17 @@
+
+export const enum Orientation { H, V };
+
+export const enum Alignment {
+		NONE     = 0,
+    LEFT     = 0x01,
+    RIGHT    = 0x02,
+    HCENTER  = 0x04,
+    JUSTIFY  = 0x08,
+    TOP      = 0x10,
+    BOTTOM   = 0x20,
+    VCENTER  = 0x40,
+    BASELINE = 0x80,
+    CENTER   = VCENTER | HCENTER,
+    H_MASK   = LEFT | RIGHT | HCENTER | JUSTIFY,
+    V_MASK   = TOP | BOTTOM | VCENTER | BASELINE,
+};
