e15fa10cdfb562955a11767afd296e8a510a6145
Max Paperno <max@wdg.us>
2023-08-06 12:15:39 -0400

    [geometry][nonatomic] Refactor and add methods to Size; Expand Rectangle with new methods; Add UnitValue class.

--- src/modules/geometry/index.ts
+++ src/modules/geometry/index.ts
@@ -1,6 +1,6 @@
 // re-exports all contained modules from a single file
 
+export { default as UnitValue } from './UnitValue'
 export { default as Rectangle } from './Rectangle'
-export { default as Size } from './Size'  // inherits Vect2d
-export { SizeType } from './Size'
 export { PointType, Point, Vect2d } from './Vect2d'
+export { SizeType, Size } from './Size'
