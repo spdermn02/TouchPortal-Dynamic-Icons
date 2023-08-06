d9bd2fded3323c3b573d78483cfab1285c69d8a8
Max Paperno <max@wdg.us>
2023-08-06 12:15:39 -0400

    [geometry][nonatomic] Refactor and add methods to Size; Expand Rectangle with new methods; Add UnitValue class.

---
+++ src/modules/geometry/UnitValue.ts
@@ -0,0 +1,37 @@
+/** A numeric value with an associated unit type. Sort-of like CSSUnitValue.
+    For now unit types are only "%" or "px", and this object just stores
+    a flag indicating if the value is relative (%) or absolute (px).
+*/
+export default class UnitValue {
+    value: number;
+    isRelative!: boolean;
+    //unit: string;  // future use?
+
+    constructor(value?: number);
+    constructor(value: number, unit: string);
+    constructor(value: number, isRelative: boolean);
+    constructor(value: number = 0, unit: any = false) {
+        this.value = value;
+        if (unit.length)
+            this.setUnit(unit);
+
+        else
+            this.setRelative(unit);
+    }
+
+    static isRelativeUnit(unit: string) {
+        return unit === "%";
+    }
+
+    setUnit(unit: string): boolean {
+        this.isRelative = UnitValue.isRelativeUnit(unit);
+        //this.unit = unit;
+        return this.isRelative;
+    }
+
+    setRelative(relative: boolean) {
+        this.isRelative = relative;
+        //this.unit = relative ? "%" : "px";
+    }
+
+}
