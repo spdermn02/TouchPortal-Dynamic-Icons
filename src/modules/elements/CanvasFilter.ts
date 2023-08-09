6a8774ec16d0379661aa71617d08f3959e48aafb
Max Paperno <max@wdg.us>
2023-08-09 10:32:11 -0400

    [CanvasFilter] Allow "update value" action on filter value (implement IValuedElement).

--- src/modules/elements/CanvasFilter.ts
+++ src/modules/elements/CanvasFilter.ts
@@ -1,17 +1,21 @@
 import { evaluateStringValue } from "../../utils/helpers";
-import { ILayerElement, RenderContext2D } from "../interfaces";
+import { ILayerElement, IValuedElement, RenderContext2D } from "../interfaces";
 import { ParseState } from "../types";
 
 // Applies a filter string to a canvas context.
-export default class CanvasFilter implements ILayerElement
+export default class CanvasFilter implements ILayerElement, IValuedElement
 {
     filter: string = "";
 
     constructor(init?: Partial<CanvasFilter>) { Object.assign(this, init); }
 
+    // ILayerElement
     get type(): string { return "CanvasFilter"; }
     // returns true if filter string is empty
     get isEmpty(): boolean { return !this.filter; }
+
+    // IValuedElement
+    setValue(value: string) { this.filter = evaluateStringValue(value); }
 
     loadFromActionData(state: ParseState): CanvasFilter {
         let atEnd = false;
@@ -20,10 +24,10 @@ export default class CanvasFilter implements ILayerElement
             const data = state.data[state.pos];
             const dataType = data.id.split('canvFilter_').at(-1);  // last part of the data ID determines its meaning
             switch (dataType) {
-                case 'filter': {
-                    this.filter = evaluateStringValue(data.value);;
+                case 'filter':
+                    this.setValue(data.value);
                     break;
-                }
+
                 default:
                     atEnd = true;  // end the loop on unknown data id
                     continue;
