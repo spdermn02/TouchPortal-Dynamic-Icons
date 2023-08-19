992457440300fb40943d242a49ba3e1e78181185
Max Paperno <max@wdg.us>
2023-08-18 20:53:59 -0400

    [main] Change default icon size to 128 x 128 (will not change any current users' defaults).

--- src/common.ts
+++ src/common.ts
@@ -15,7 +15,7 @@ export function setCommonLogger(logger: Function) {
 // Runtime options.
 export const PluginSettings = {
     // these can be changed in TP Settings
-    defaultIconSize: <SizeType> { width: 256, height: 256 },
+    defaultIconSize: <SizeType> { width: 128, height: 128 },
     defaultGpuRendering: <boolean> true,
     defaultOutputCompressionLevel: <number> 4,  // MP: 4 seems to give the highest effective compression in my tests, no gains with higher levels but does slow down.
 };
