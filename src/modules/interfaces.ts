12d653d1d10a909767860bab1c4e8bdec9e9b0f5
Max Paperno <max@wdg.us>
2023-09-25 00:26:15 -0400

    [types] Move skia-canvas type exports from interfaces.ts to types.ts.

--- src/modules/interfaces.ts
+++ src/modules/interfaces.ts
@@ -1,9 +1,7 @@
 
 import { Rectangle } from './geometry';
-// We specifically need the CanvasRenderingContext2D from Skia, not the default one, since it has methods we use which the default version doesn't have.
-// The idea is to centralize the context type here which IRenderable::render() needs, instead of inside each implementation of the interface. Maybe there's a better way?
-import type { CanvasRenderingContext2D as SkiaContext2D } from 'skia-canvas'
-export type RenderContext2D = SkiaContext2D
+import { Path2D, RenderContext2D } from './types';
+export { RenderContext2D };  // to be removed once all element imports are updated
 
 // Represents an abstract item which can be drawn onto, or otherwise affect (eg. style, transform), a canvas context.
 export interface IRenderable {
