582eb0a38bea61d59e2b7a40dea4263e04c2563a
Max Paperno <max@wdg.us>
2023-09-30 08:09:18 -0400

    [StrokeStyle] Add optional Rectangle argument to `render()` method to auto-scale line width; Add proper scaling for line dash and dash offset properties when rendering; Round scaled line width (and dashes) to 4 decimals.

--- src/modules/elements/StrokeStyle.ts
+++ src/modules/elements/StrokeStyle.ts
@@ -1,7 +1,7 @@
 
-import { RenderContext2D, ParseState, UnitValue } from '..';
+import { ParseState, Rectangle, RenderContext2D, UnitValue } from '..';
 import { BrushStyle } from '.';
-import { assignExistingProperties, evaluateValue, parseNumericArrayString } from '../../utils';
+import { assignExistingProperties, evaluateValue, parseNumericArrayString, round4p } from '../../utils';
 
 // Applies a stroke (line) style to a canvas context, which includes stroke style, line width, pen, cap, join, miter, and dash array.
 export default class StrokeStyle
@@ -20,7 +20,9 @@ export default class StrokeStyle
 
     // returns true if color is invalid or line width is zero
     get isEmpty(): boolean { return this.scaledWidth <= 0 || this.pen.isNull; }
-    get scaledWidth(): number { return this.width.isRelative ? this.width.value * this.widthScale : this.width.value; }
+    get scaledWidth(): number { return this.width.isRelative ? round4p(this.width.value * this.widthScale) : this.width.value; }
+    get scaledLineDash(): number[] { return this.width.isRelative ? this.lineDash.map(v => round4p(v * this.widthScale)) : this.lineDash; }
+    get scaledDashOffset(): number { return this.width.isRelative && this.dashOffset ? round4p(this.dashOffset * this.widthScale) : this.dashOffset; }
 
     constructor(init?: Partial<StrokeStyle> | any ) {
         if (init?.width != undefined)
@@ -42,7 +44,7 @@ export default class StrokeStyle
         // the incoming data IDs should be structured with a naming convention
         for (let e = state.data.length; state.pos < e && !atEnd; ) {
             const data = state.data[state.pos];
-            const dataType = data.id.split(dataIdPrefix + 'line_').at(-1);  // last part of the data ID determines its meaning
+            const dataType = data?.id.split(dataIdPrefix + 'line_').at(-1);  // last part of the data ID determines its meaning
             switch (dataType) {
                 case 'width':
                     this.width.value = Math.abs(evaluateValue(data.value));
@@ -79,17 +81,19 @@ export default class StrokeStyle
     }
 
     // IRenderable
-    render(ctx: RenderContext2D): void {
-        if (!ctx || this.isEmpty)
+    render(ctx: RenderContext2D, rect?: Rectangle): void {
+        if (this.isEmpty)
             return;
+        if (rect && this.width.isRelative && !rect.isEmpty)
+            this.widthScale = Math.min(rect.width, rect.height) * .005
         ctx.lineWidth = this.scaledWidth;
         ctx.strokeStyle = this.pen.style;
         ctx.lineCap = this.cap;
         ctx.lineJoin = this.join;
         ctx.miterLimit = this.miterLimit;
         if (this.lineDash.length) {
-            ctx.lineDashOffset = this.dashOffset;
-            ctx.setLineDash(this.lineDash);
+            ctx.lineDashOffset = this.scaledDashOffset;
+            ctx.setLineDash(this.scaledLineDash);
         }
     }
 }
