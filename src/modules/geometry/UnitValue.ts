a68ee6c3101d4924940f17fc0db33ab14406ddce
Max Paperno <max@wdg.us>
2025-04-02 20:57:41 -0400

    [UnitValue] Add ability to set value and unit from a single string.

--- src/modules/geometry/UnitValue.ts
+++ src/modules/geometry/UnitValue.ts
@@ -3,24 +3,34 @@
     a flag indicating if the value is relative (%) or absolute (px).
 */
 export default class UnitValue extends Object {
-    value: number;
-    isRelative!: boolean;
+    value: number = 0;
+    isRelative: boolean = false;
     unit!: string;
 
     constructor(value?: number);
+    constructor(value: string);
     constructor(value: number, unit: string);
     constructor(value: number, isRelative: boolean);
-    constructor(value: number = 0, unit: any = false) {
+    constructor(value: number | string = 0, unit: any = false) {
         super();
-        this.value = value;
-        if (unit.length)
-            this.setUnit(unit);
-        else
-            this.setRelative(unit);
+        if (typeof value == 'number') {
+            this.value = value;
+            if (unit.length)
+                this.setUnit(unit);
+            else
+                this.setRelative(unit);
+            return;
+        }
+        if (typeof value == 'string')
+            this.setFromString(value);
     }
 
     static isRelativeUnit(unit: string) {
         return unit === "%";
+    }
+
+    static fromString(value: string): UnitValue {
+        return new UnitValue().setFromString(value);
     }
 
     setUnit(unit: string): boolean {
@@ -32,6 +42,16 @@ export default class UnitValue extends Object {
     setRelative(relative: boolean) {
         this.isRelative = relative;
         this.unit = relative ? "%" : "px";
+    }
+
+    setFromString(value: string) {
+        let v:any = parseInt(value);
+        if (v != Number.NaN)
+            this.value = v;
+        v = value.replace(/\D/, '');
+        if (v)
+            this.setUnit(v);
+        return this;
     }
 
     override toString(): string {
