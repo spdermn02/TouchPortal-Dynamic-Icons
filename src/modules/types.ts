5f533e2f630f2072a543d841bbaf056d3fe6ba51
Max Paperno <max@wdg.us>
2025-04-10 19:12:34 -0400

    [types] Export skia-canvas fill/stroke style objects and create unified `ContextFillStrokeType`.

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -12,11 +12,14 @@ export type ConstructorType<T> = new(...args:any[])=>T;
 /** Defines how to resize images (or other block elements). Equivalent to CSS `object-fit` property. */
 export type ResizeFitOption = "contain" | "cover" | "fill" | "scale-down" | "none";
 
+import type { CanvasGradient, CanvasPattern, CanvasTexture } from 'skia-canvas';
+export type ContextFillStrokeType = string | CanvasGradient | CanvasPattern | CanvasTexture;
+
 // We specifically need the CanvasRenderingContext2D and Path2D from Skia, not the default definitions, since they have methods we use which the default versions doesn't.
 // The idea is to centralize the types here which other modules need, instead of importing from skia-canvas inside each implementation.
 // This should also make is easier to change provider libraries if necessary.
 export {
-    Canvas, CanvasRenderingContext2D,
+    Canvas, CanvasGradient, CanvasPattern, CanvasRenderingContext2D, CanvasTexture,
     DOMMatrix, DOMPoint, DOMRect,
     Image, ImageData, loadImage, loadImageData,
     Path2D, TextMetrics,
