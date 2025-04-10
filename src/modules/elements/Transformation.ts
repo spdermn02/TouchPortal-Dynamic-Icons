b43fa005cadaa76918d7aeb8640e8354849a6a1c
Max Paperno <max@wdg.us>
2025-04-09 21:16:50 -0400

    [elements][Transformation] Refactor properties to private scope and add accessors which handle change tracking and cache reset; Implement loading properties from action data record object; Docs.

--- src/modules/elements/Transformation.ts
+++ src/modules/elements/Transformation.ts
@@ -1,203 +1,216 @@
 
 import { ILayerElement, IRenderable } from '../interfaces';
-import {
-    Canvas, DOMMatrix, LayerRole, Path2D, ParseState, Point, PointType,
-    Rectangle, RenderContext2D, Size, SizeType, TransformOpType
-} from '../';
+import { Canvas, DOMMatrix, LayerRole, Point, Size, TransformOpType, Vect2d } from '../';
 import { DEFAULT_TRANSFORM_OP_ORDER } from '../../utils/consts';
-import { arraysMatchExactly, evaluateValue, fuzzyEquals4p, round4p /* , parsePointFromValue */ } from '../../utils';
+import { assignExistingProperties, arraysMatchExactly, evaluateValue, fuzzyEquals4p, round4p } from '../../utils';
+import type { ParseState, Path2D, PointType, Rectangle, RenderContext2D, SizeType, TpActionDataRecord } from '../';
 
 export const enum TransformScope {
-    PreviousOne,  // affects only the layer before the transform
-    Cumulative,   // affects all previous layers drawn so far
-    UntilReset,   // affects all layers until an empty transform (or end)
-    Reset,        // resets one previous `UntilReset` transform
+    /** affects only the layer before the transform */
+    PreviousOne,
+    /** affects all previous layers drawn so far */
+    Cumulative,
+    /** affects all layers until an empty transform (or end) */
+    UntilReset,
+    /** resets one previous `UntilReset` transform */
+    Reset,
 }
 
+/** Applies a matrix transformation to a canvas context.
+
+Transform operations are specified as individual steps (eg. rotate, translate) and then combined in the specified {@link order}
+into a {@link DOMMatrix} which can then be queried with {@link toMatrix} and {@link getMatrix} or
+applied to a context or a `Path2D` path in the {@link render} or {@link transformPaths} methods, respectively.
+
+The primary difference between this class and a regular `DOMMatrix` (or transforming a canvas directly) is that all the terms
+used in `Transformation` are relative to a given size when queried or drawn. The values for all operations are specified as percentages,
+not literal pixels. The query and drawing methods all require a rectangle (or size and origin point) to scale the final matrix into.
+
+It employs a caching strategy to avoid re-computing the final matrix if input parameters and the drawing bounds do not change between
+calls to the `render()`, `transformPaths()` or `getMatrix()` methods.
+*/
 export default class Transformation implements ILayerElement, IRenderable
 {
-    // Coordinates are stored as decimals as used in matrix transform operations.
-    rotate: number = 0; // percent of 360 degrees
-    scale: PointType = Point.new(); // percent of requested image size (not the original source image), eg: 2.0 is double size, 0.5 is half size.
-    translate: PointType = Point.new(); // percentage of relevant dimension of requested image size
-                                        // eg: x = 1 translates one full image width to the right (completely out of frame for an unscaled source image)
-    skew: PointType = Point.new(); // percent of requested image size (not the original source image)
-    transformOrder: TransformOpType[] = DEFAULT_TRANSFORM_OP_ORDER;  // careful! reference... don't edit, replace entirely.
-    scope: TransformScope = TransformScope.PreviousOne;
-
-    private cache = {
+    #rotate = 0;                   // percent of 360 degrees
+    #scale = new Vect2d(100, 100); // percentage of relevant dimension of requested size
+    #translate = new Vect2d();     //
+    #skew = new Vect2d();          //
+    #transformOrder = DEFAULT_TRANSFORM_OP_ORDER;  // careful! reference... don't edit, replace entirely.
+    #scope = TransformScope.PreviousOne;
+    #cache = {
         matrix: <DOMMatrix | null> null,
         origin: <PointType | null> null,
         size: <SizeType | null> null,
     }
 
-    constructor(init?: Partial<Transformation>) { Object.assign(this, init); }
+    constructor(init?: Partial<Transformation>) {
+        assignExistingProperties(this, init, 0);
+    }
 
     // ILayerElement
-    readonly type = "Transformation";
+    /** @internal */
     readonly layerRole: LayerRole = LayerRole.Transform;
 
+    /** Returns `true` if this transform has no operations to perform. */
     get isEmpty(): boolean {
-        return this.scope == TransformScope.Reset || (fuzzyEquals4p(this.rotate, 0) && Point.fuzzyIsNull(this.translate) && Point.fuzzyIsNull(this.skew) && !this.isScaling);
+        return this.#scope == TransformScope.Reset || (fuzzyEquals4p(this.#rotate, 0) && this.#translate.fuzzyIsNull && this.#skew.fuzzyIsNull && !this.isScaling);
     }
+    /** Returns `true` if this transformation has a scaling component. */
     get isScaling(): boolean {
-        return !Point.fuzzyEquals(this.scale, {x:100,y:100});
+        return !this.#scale.fuzzyIsNull;
     }
 
-    loadFromActionData(state: ParseState): Transformation {
-        // the incoming data IDs should be structured with a naming convention
-        // For properties with X,Y values, this currently can handle both single and double-field versions (X and Y are separate fields),
-        // though currently no actions use the single field variant so this could be trimmed down if no use is found for that option.
-        let atEnd = false, dirty = false, tmp;
-        for (const e = state.data.length; state.pos < e && !atEnd;) {
-            const data = state.data[state.pos];
-            const dataType = data.id.split('tx_').at(-1);  // last part of the data ID determines its meaning
-            switch (dataType) {
-                case 'rot':
-                    if (this.rotate != (tmp = evaluateValue(data.value))) {
-                        this.rotate = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'trsX':
-                    if (this.translate.x != (tmp = evaluateValue(data.value))) {
-                        this.translate.x = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'trsY':
-                    if (this.translate.y != (tmp = data.value.trim() ? evaluateValue(data.value) : this.translate.x)) {
-                        this.translate.y = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'sclX':
-                    if (this.scale.x != (tmp = evaluateValue(data.value))) {
-                        this.scale.x = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'sclY':
-                    if (this.scale.y != (tmp = data.value.trim() ? evaluateValue(data.value) : this.scale.x)) {
-                        this.scale.y = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'skwX':
-                    if (this.skew.x != (tmp = evaluateValue(data.value))) {
-                        this.skew.x = tmp;
-                        dirty = true;
-                    }
-                    break;
-                case 'skwY':
-                    if (this.skew.y != (tmp = data.value.trim() ? evaluateValue(data.value) : this.skew.x)) {
-                        this.skew.y = tmp;
-                        dirty = true;
-                    }
-                    break;
-                /* these 3 cases allow for X[,Y] coordinates to be specified in one data field, however they're currently unused by any action
-                case 'trs':
-                    Point.set(this.translate, parsePointFromValue(data.value));
-                    break;
-                case 'scl':
-                    Point.set(this.scale, parsePointFromValue(data.value));
-                    break;
-                case 'skw':
-                    Point.set(this.skew, parsePointFromValue(data.value));
-                    break;
-                */
-                case 'order': {
-                    if (data.value) {
-                        const order = data.value.split(', ') as TransformOpType[];
-                        if (!arraysMatchExactly(this.transformOrder, order)) {
-                            this.transformOrder = order;
-                            dirty = true;
-                        }
-                    }
-                    break;
-                }
-                case 'scope': {
-                    let scope: TransformScope | null = null;
-                    // "previous layer", "all previous", "all following", "reset following"
-                    if (data.value[0] == 'p')
-                        scope = TransformScope.PreviousOne;
-                    else if (data.value[4] == 'p')
-                        scope = TransformScope.Cumulative;
-                    else if (data.value[4] == 'f')
-                        scope = TransformScope.UntilReset;
-                    else if (data.value[0] == 'r')
-                        scope = TransformScope.Reset;
-                    if (scope && scope != this.scope) {
-                        this.scope = scope;
-                        dirty = true;
-                    }
-                    break;
-                }
-                default:
-                    atEnd = true;
-                    continue;
-            }
-            ++state.pos;
+    /** Rotation component of the transform expressed as percent of 360 degrees (eg. 50 % = 180 degrees). */
+    get rotate() { return this.#rotate; }
+    set rotate(percent: number) {
+        if (percent != this.#rotate) {
+            this.#rotate = percent;
+            this.clearCache();
         }
-        if (dirty)
-            this.cache.matrix = null;
+    }
+
+    /** Translation component expressed as percentage of relevant dimension of requested size; eg: x = 100 translates one full width to the right. */
+    get translate() { return this.#translate; }
+    set translate(percent: PointType) {
+        if (!this.#translate.equals(percent)) {
+            this.#translate.set(percent);
+            this.clearCache();
+        }
+    }
+
+    /** Scaling component expressed as percent of requested size, eg: 200.0 is double size, 50 is half size. */
+    get scale() { return this.#scale; }
+    set scale(percent: PointType) {
+        if (!this.#scale.equals(percent)) {
+            this.#scale.set(percent);
+            this.clearCache();
+        }
+    }
+
+    /** Skewing component expressed as percent of requested size. */
+    get skew() { return this.#skew; }
+    set skew(percent: PointType) {
+        if (!this.#skew.equals(percent)) {
+            this.#skew.set(percent);
+            this.clearCache();
+        }
+    }
+
+    /** The order in which to apply transform component operations. Expressed as an array of operations where each operation is one of:
+    ```
+        "O"  - Offset (translate)
+        "R"  - Rotate
+        "SC" - Scale
+        "SK" - Skew
+    ```
+    */
+    get order() { return this.#transformOrder; }
+    set order(order: TransformOpType[]) {
+        if (!arraysMatchExactly(this.#transformOrder, order)) {
+            this.#transformOrder = order;
+            this.clearCache();
+        }
+    }
+
+    /** The transformation scope affects which layer(s) of a layered icon or paths of a paths array are affected by this transform.
+    Valid string values for setting this property are one of: "previous layer", "all previous", "all following", "reset [following]"
+    Only relevant when using {@link render} or {@link transformPaths} methods. For `transformPaths()`, only the first two scopes are relevant.
+    When reading the property it is always returned as a numeric enumeration type.
+    */
+    get scope(): TransformScope { return this.#scope; }
+    set scope(scope: TransformScope | string) {
+        if (typeof scope == 'string') {
+            // "previous layer", "all previous", "all following", "reset following"
+            if (scope[0] == 'p')
+                scope = TransformScope.PreviousOne;
+            else if (scope[4] == 'p')
+                scope = TransformScope.Cumulative;
+            else if (scope[4] == 'f')
+                scope = TransformScope.UntilReset;
+            else if (scope[0] == 'r')
+                scope = TransformScope.Reset;
+            else
+                return;
+        }
+        this.#scope = scope;
+    }
+
+    /** Clears the cached transform matrix. Typically the cache management is handled automatically when relevant properties are modified. */
+    clearCache() { this.#cache.matrix = null; }
+
+    /** Returns `true` if any properties were modified or cached matrix hasn't been generated yet.
+        @internal  */
+    loadFromDataRecord(dr: TpActionDataRecord): boolean {
+        if (dr.rot)
+            this.rotate = evaluateValue(dr.rot);
+        if (dr.trsX)
+            this.translate = Point.new(evaluateValue(dr.trsX), evaluateValue(dr.trsY, null));
+        if (dr.sclX)
+            this.scale = Point.new(evaluateValue(dr.sclX), evaluateValue(dr.sclY, null));
+        if (dr.skwX)
+            this.skew = Point.new(evaluateValue(dr.skwX), evaluateValue(dr.skwY, null));
+        if (dr.order)
+            this.order = dr.order.split(', ') as TransformOpType[];
+        if (dr.scope)
+            this.scope = dr.scope;
         // console.dir(this);
+        return this.#cache.matrix == null;
+    }
+
+    /** @internal */
+    loadFromActionData(state: ParseState /* , statePrefix: string = Act.IconTx */): this {
+        // const dr = state.asRecord(state.pos, statePrefix + Str.IdSep);
+        this.loadFromDataRecord(state.dr);
         return this;
     }
 
-    /** Returns the current transform operations as a `DOMMatrix` which uses given `txOrigin` as origin point for rotatoin and scaling,
+    /** Returns the current transform operations as a `DOMMatrix` which uses given `txOrigin` as origin point for rotation and scaling,
         and scales translations to `txArea` size.  The returned value may be a cached version if no properties have changed since the cache was created,
         and `txOrigin` and `txArea` are the same as the cache'd version. Using this method with a new arguments will regenerate the matrix and update the cache. */
     getMatrix(txOrigin: PointType, txArea: SizeType) {
-        if (!this.cache.matrix || !this.cache.origin || !this.cache.size || !Point.fuzzyEquals(this.cache.origin, txOrigin) || !Size.fuzzyEquals(this.cache.size, txArea)) {
-            this.cache.matrix = this.toMatrix(txOrigin, txArea);
-            this.cache.origin = txOrigin;
-            this.cache.size = txArea;
+        if (!this.#cache.matrix || !this.#cache.origin || !this.#cache.size || !Point.fuzzyEquals(this.#cache.origin, txOrigin) || !Size.fuzzyEquals(this.#cache.size, txArea)) {
+            this.#cache.matrix = this.toMatrix(txOrigin, txArea);
+            this.#cache.origin = txOrigin;
+            this.#cache.size = txArea;
         }
-        return this.cache.matrix;
+        return this.#cache.matrix;
     }
 
-    /** Creates a DOMMatrix from the current transform options which uses given `txOrigin` as transform origin and scales translations to `txArea` size.
+    /** Creates a `DOMMatrix` from the current transform options which uses given `txOrigin` as transform origin and scales translations to `txArea` size.
         This method does not use any cached matrix but always generates a new one. */
     toMatrix(txOrigin: PointType, txArea: SizeType): DOMMatrix {
         const m = new DOMMatrix();
-        for (const op of this.transformOrder) {
+        for (const op of this.#transformOrder) {
             switch (op) {
                 case TransformOpType.Rotate:
-                    if (!fuzzyEquals4p(this.rotate, 0)) {
-                        // rotate from origin point
-                        m.translateSelf(txOrigin.x, txOrigin.y);
-                        m.rotateSelf(0, 0, round4p(this.rotate * .01 * 360));
-                        m.translateSelf(-txOrigin.x, -txOrigin.y);
-                    }
+                    if (!fuzzyEquals4p(this.#rotate, 0))
+                        m.rotateZSelf(round4p(this.#rotate * .01 * 360), txOrigin);
                     break;
                 case TransformOpType.Offset:
-                    if (!Point.fuzzyIsNull(this.translate))
-                        m.translateSelf(round4p(this.translate.x * .01 * txArea.width), round4p(this.translate.y * .01 * txArea.height), 0);
+                    if (!this.#translate.fuzzyIsNull)
+                        m.translateSelf(round4p(this.#translate.x * .01 * txArea.width), round4p(this.#translate.y * .01 * txArea.height), 0);
                     break;
                 case TransformOpType.Scale:
                     if (this.isScaling)
-                        m.scaleSelf(round4p(this.scale.x * .01), round4p(this.scale.y * .01), 0, txOrigin.x, txOrigin.y, 0);
+                        m.scaleSelf(round4p(this.#scale.x * .01), round4p(this.#scale.y * .01), 0, txOrigin.x, txOrigin.y, 0);
                     break;
                 case TransformOpType.Skew:
-                    if (!fuzzyEquals4p(this.skew.x, 0))
-                        m.skewXSelf(round4p(this.skew.x * .01));
-                    if (!fuzzyEquals4p(this.skew.y, 0))
-                        m.skewYSelf(round4p(this.skew.y * .01));
+                    if (!this.#skew.fuzzyIsNull)
+                        m.skewSelf(round4p(this.#skew.x * .01 * txArea.width), round4p(this.#skew.y * .01 * txArea.height), txOrigin);
                     break;
             }
         }
         return m;
     }
 
-    // ILayerElement
-    // Applies current transform matrix to the given canvas context using `rect` coordinates for tx center origin and area.
+    // IRenderable
+    /** Applies current transform matrix to the given canvas context using `rect` coordinates for tx center origin and area. */
     render(ctx: RenderContext2D, rect: Rectangle) : void {
          if (this.isEmpty)
             return;
 
         // For a cumulative ("everything above") type Tx we need to apply it to a new canvas/context and then afterwards we draw the original canvas on top.
-        if (this.scope == TransformScope.Cumulative) {
+        if (this.#scope == TransformScope.Cumulative) {
             // Here we need to copy anything drawn previously onto the new transformed context/canvas.
             // It may be clever to just switch up the context reference that is getting passed around
             // to all the render() methods... but that just seems wrong on several levels.
@@ -215,13 +228,17 @@ export default class Transformation implements ILayerElement, IRenderable
         }
     }
 
-    // IPathHandler
+    /** Applies current transform matrix to the given `paths` starting at `fromIdx` and using `rect.size` for relative scaling and translation.
+        The individual path bounds are used for computing center point for rotations and scaling.
+
+        If the `scope` of this transform is `TransformScope.PreviousOne` then only the last path in `paths` array is tranformed, regardless of how many there are in total.
+    */
     transformPaths(paths: Path2D[], _: RenderContext2D, rect: Rectangle, fromIdx: number = 0): void {
         const len = paths.length;
         if (!len || fromIdx < 0 || fromIdx >= len || this.isEmpty)
             return;
 
-        if (this.scope == TransformScope.PreviousOne)
+        if (this.#scope == TransformScope.PreviousOne)
             fromIdx = len - 1;
 
         for ( ; fromIdx < len; ++fromIdx) {
