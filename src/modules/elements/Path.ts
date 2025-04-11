d0602b0aacbcef8b0b1e9982fe8d8e36e23f3a63
Max Paperno <max@wdg.us>
2025-04-10 20:02:06 -0400

    [elements][Path][non-atomic] Clean up constructor; Add `clearCache()` method; Docs.

--- src/modules/elements/Path.ts
+++ src/modules/elements/Path.ts
@@ -15,17 +15,27 @@ export class PathCache {
     }
 }
 
-/** Base class for Path elements. Extends SizedElement with `operation` property and provides helper methods. */
+/** Base class for Path elements. Extends {@link SizedElement} with {@link operation} property and provides helper methods. */
 export default class Path extends SizedElement
 {
-    operation: PathBoolOperation = PathBoolOperation.None;  // boolean operation to perform with previous path, if any
+    /** Boolean operation to perform with previous path, if any.
+        May be used by subclasses in their `IPathProducer#getPath` method to automatically combine with other paths. */
+    operation: PathBoolOperation = PathBoolOperation.None;
+    /** Cache for generated `Path3D` objects, possibly scaled to a particular size. May be used by subclasses.
+        @see {@link clearCache}. */
     protected readonly cache: PathCache = new PathCache();
 
     readonly layerRole: LayerRole = LayerRole.PathProducer;
 
-    constructor(init?: Partial<Path> | any) {
-        super(init);
-        assignExistingProperties(this, init, 0);
+    constructor(init?: Partial<Path>) {
+        super();
+        assignExistingProperties(this, init, 1);
+    }
+
+    /** Clears the generated & cached Path2D object (if any). Some `Path` subclasses my not use the cache.
+        Typically the cache management is handled automatically when relevant properties are modified. */
+    clearCahe() {
+        this.cache.clear();
     }
 
     /** Parses a string value into an `PathBoolOperation` enum type result and returns it, or PathBoolOperation.None if the value wasn't valid. */
@@ -37,7 +47,7 @@ export default class Path extends SizedElement
         return ret;
     }
 
-    /** Returns true if any properties were changed. */
+    /** @internal  Returns true if any parent `SizedElement` properties were changed. */
     protected loadFromDataRecord(dr: TpActionDataRecord): boolean
     {
         if (dr.operation) {
