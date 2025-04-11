f15d905a4a7704d85fac53bb8c1336c7c5d284bd
Max Paperno <max@wdg.us>
2025-04-10 20:26:31 -0400

    [elements][RoundProgressGauge][non-atomic] Load action data from data object and use it for line style parser; Add constructor; Remove `type` property and add `layerRole`; Docs.

--- src/modules/elements/RoundProgressGauge.ts
+++ src/modules/elements/RoundProgressGauge.ts
@@ -1,32 +1,48 @@
 
-import { ArcDrawDirection, ColorUpdateType } from '../';
+import { ColorUpdateType, ArcDrawDirection, LayerRole, ParseState } from '../';
 import { StrokeStyle, BrushStyle } from './';
-import { clamp, evaluateValue, parseArcDirection } from '../../utils';
+import { assignExistingProperties, evaluateValue, clamp, parseArcDirection } from '../../utils';
 import { M } from '../../utils/consts';
 import type { IColorElement, ILayerElement, IRenderable, IValuedElement } from '../interfaces';
-import type { ParseState, Rectangle, RenderContext2D } from '../';
+import type { Rectangle, RenderContext2D } from '../';
 
-// Draws an arc/circle extending from 0 to 360 degrees based on a given value onto a canvas context.
+/** Draws an arc/circle extending from 0 to 360 degrees based on a given value onto a canvas context. */
 export default class RoundProgressGauge implements ILayerElement, IRenderable, IValuedElement, IColorElement
 {
-    value: number = 0    // decimal percent, -1 through 1
+    /** Gauge value as decimal percent, -1 through 1 */
+    value: number = 0
+    /** A highlight is a copy of the value track with a blur filter applied, drawn behind the value track itself and with the same color. */
     highlightOn = true
-    shadowColor = new BrushStyle('#282828FF')
-    startAngle = 180 * M.D2R  // radians
+    /** Shadow color to draw behind the gauge area. Use any transparent color to disable. */
+    readonly shadowColor = new BrushStyle('#282828FF')
+    /** Starting angle in radians. 0 points east, default is west. */
+    startAngle = 180 * M.D2R
+    /** Drawing direction, clockwise (0), counter-clockwise (1), or automatic (2) based on value being positive (CW) or negative (CCW). */
     direction: ArcDrawDirection = ArcDrawDirection.CW
-    backgroundColor = new BrushStyle('#000000FF')
+    /** The background follows the radius of the gauge, not the full icon drawing area. */
+    readonly backgroundColor = new BrushStyle('#000000FF')
+    /** Radius of gauge arc, expressed as decimal percentage of half of the overall drawing size (eg. radius of 0.5 would be one quarter of the overall image size). */
     radius = 0.39  // approximates the original ratio of 100 for 256px icon size
-    lineStyle: StrokeStyle = new StrokeStyle({
+    /** Style to use for drawing the value track. (The track cannot be filled, only stroked.) */
+    readonly lineStyle: StrokeStyle = new StrokeStyle({
         width: 11.7,   // this line width preserves legacy default size before it was settable
         cap: "round"
     })
 
+    constructor(init?: Partial<RoundProgressGauge>) {
+        assignExistingProperties(this, init, 1);
+    }
+
     // ILayerElement
-    readonly type = "RoundProgressGauge"
+    /** @internal */
+    readonly layerRole: LayerRole = LayerRole.Drawable;
+
     // IValuedElement
+    /** Sets {@link RoundProgressGauge#value} property using an evaluated string. */
     setValue(value: string) { this.value = evaluateValue(value) * .01; }
 
     // IColorElement
+    /** @internal */
     setColor(value: string, type: ColorUpdateType): void {
         switch (type) {
             case ColorUpdateType.Foreground:
@@ -41,69 +57,60 @@ export default class RoundProgressGauge implements ILayerElement, IRenderable, I
         }
     }
 
+    // ILayerElement
+    /** @internal */
     loadFromActionData(state: ParseState): RoundProgressGauge {
-        // the incoming data IDs should be structured with a naming convention
-        let lineParsed = false,
-            atEnd = false,
-            shadowOff = false
-        for (let e = state.data.length; state.pos < e && !atEnd;) {
-            const data = state.data[state.pos]
-            const dataType = data.id.split('gauge_').at(-1);  // last part of the data ID determines its meaning
-            switch (dataType) {
+        const dr = state.asRecord(state.pos, "gauge_");
+        let shadowOff = false;
+        // console.dir(dr);
+        for (const [key, value] of Object.entries(dr)) {
+            switch (key) {
                 case 'color':
-                    this.lineStyle.pen.color = data.value
+                    this.lineStyle.pen.color = value
                     break
                 case 'highlight':
-                    this.highlightOn = (data.value === "On")
+                    this.highlightOn = (value === "On")
                     break
                 case 'start_degree':
-                    this.startAngle = evaluateValue(data.value) * M.D2R
+                    this.startAngle = evaluateValue(value) * M.D2R
                     break
                 case 'value':
-                    this.setValue(data.value)
+                    this.setValue(value)
                     break
                 case 'radius':
-                    this.radius = clamp(evaluateValue(data.value), 2, 200) * .005
+                    this.radius = clamp(evaluateValue(value), 2, 200) * .005
                     break
                 case 'counterclockwise':
-                    this.direction = parseArcDirection(data.value)
+                    this.direction = parseArcDirection(value)
                     break
                 case 'background_color':
-                    this.backgroundColor.color = data.value
+                    this.backgroundColor.color = value
                     break
                 case 'shadow_color':
-                    this.shadowColor.color = data.value
+                    this.shadowColor.color = value
                     break
-
                 // legacy options, keep for BC with <v1.2
                 case 'shadow':
-                    shadowOff = (data.value == "Off")
+                    // force transparent shadow color if using old action with a shadow toggle option
+                    shadowOff = (value == "Off")
                     break
                 case 'cap':
-                    if (!lineParsed)
-                        this.lineStyle.cap = data.value as CanvasLineCap
+                    this.lineStyle.cap = <CanvasLineCap>value
                     break
-
                 default:
-                    if (!lineParsed && dataType?.startsWith('line_')) {
-                        this.lineStyle.loadFromActionData(state)
-                        lineParsed = true
-                    }
-                    else {
-                        atEnd = true
-                    }
-                    continue  // do not increment position counter
+                    continue;
             }
-            ++state.pos;
+            delete dr[key];
         }
-        // force transparent shadow color if using old action with a shadow toggle option
+        this.lineStyle.loadFromDataRecord(ParseState.splitRecordKeys(dr, "line_"));
         if (shadowOff)
             this.shadowColor.color = "#00000000"
         // console.dir(this);
         return this;
     }
 
-    // ILayerElement
+    // IRenderable
+    /** Draws this gauge at its current `value` property onto `ctx` using `rect` dimensions for scaling. */
     render(ctx: RenderContext2D, rect: Rectangle): void {
 
         let ccw = this.direction == ArcDrawDirection.CCW  // check to invert the value for endAngle
