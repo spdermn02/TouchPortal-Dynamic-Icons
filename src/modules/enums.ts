3f6406ebb9f35494768ffbe668ce258603627db7
Max Paperno <max@wdg.us>
2025-05-06 15:25:21 -0400

    [enums] Add Alignment enum aliases for various combinations; Add docs.

--- src/modules/enums.ts
+++ src/modules/enums.ts
@@ -1,4 +1,5 @@
 
+/** @internal */
 export const enum LayerRole {
     None         = 0,
     Drawable     = 0x01,
@@ -7,31 +8,51 @@ export const enum LayerRole {
     PathConsumer = 0x08,
 }
 
+/** Describes the orientation of a linear element. */
 export const enum Orientation { H, V };
 
+/** Describes the drawing direction for a circular path (arc, ellipse, etc). `Auto` value meaning depends on implementation. */
 export const enum ArcDrawDirection { CW, CCW, Auto }
 
+/** Describes the location of an element in relation to another. */
 export const enum Placement {
     NoPlace, Inside, Outside, Center,
     TopLeft = Inside,
     BottomRight = Outside,
 }
 
+/** Describes horizontal and vertical alignment values. */
 export const enum Alignment {
     NONE     = 0,
+    // horizontal
     LEFT     = 0x01,
     RIGHT    = 0x02,
     HCENTER  = 0x04,
     JUSTIFY  = 0x08,
+    H_MASK   = 0x0F,  // mask
+    // vertical
     TOP      = 0x10,
     BOTTOM   = 0x20,
     VCENTER  = 0x40,
     BASELINE = 0x80,
-    CENTER   = VCENTER | HCENTER,
-    H_MASK   = LEFT | RIGHT | HCENTER | JUSTIFY,
-    V_MASK   = TOP | BOTTOM | VCENTER | BASELINE,
+    V_MASK   = 0xF0,  // mask
+    // aliases
+    CENTER    = VCENTER | HCENTER,
+    TopLeft   = TOP | LEFT,
+    TopCenter = TOP | HCENTER,
+    TopCtr    = TopCenter,
+    TopRight  = TOP | RIGHT,
+    MidLeft   = VCENTER | LEFT,
+    MidCenter = VCENTER | HCENTER,
+    MidCtr    = MidCenter,
+    MidRight  = VCENTER | RIGHT,
+    BotLeft   = BOTTOM | LEFT,
+    BotCenter = BOTTOM | HCENTER,
+    BotCtr    = BotCenter,
+    BotRight  = BOTTOM | RIGHT,
 };
 
+/** Path combining operations for {@link Path} type and subclasses. */
 export const enum PathBoolOperation {
     None       = "none",
     Add        = "add",  // actually `addPath()`
@@ -42,6 +63,7 @@ export const enum PathBoolOperation {
     Xor        = "xor"
 }
 
+/** Transformation operation type, used by {@link Transformation }. */
 export const enum TransformOpType {
     Offset = 'O',
     Rotate = 'R',
@@ -49,6 +71,9 @@ export const enum TransformOpType {
     Skew = 'SK',
 }
 
+/** Used by elements supporting the `setColor(value: string, type: ColorUpdateType)` method (`IColorElement` interface).
+    @internal
+*/
 export const enum ColorUpdateType {
     None      = 0,
     Stroke    = 0x01,
